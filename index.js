157ca4a7c998da13b9793f458b1830a66b5cdef3
ilsun <naiseya486@gmail.com>
2020-09-13 20:21:06 +0900

    dotenv success

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var mongoose = require('mongoose');
-//var dotenv = require('dotenv');
+var dotenv = require('dotenv');
+dotenv.config();
 var app = express();
-//dotenv.config
 
 //DB setting 
 mongoose.set('useNewUrlParser', true);
@@ -10,8 +10,8 @@ mongoose.set('useFindAndModify', false);
 mongoose.set('useCreateIndex', true);
 mongoose.set('useUnifiedTopology', true);
 
-//mongoose.connect(process.env.MONGO_URL);
-mongoose.connect("mongodb://127.0.0.1:27017/contacts_db");
+mongoose.connect(process.env.MONGO_URL);
+// mongoose.connect("mongodb://127.0.0.1:27017/contacts_db");
 var db = mongoose.connection;
 db.once('open', function(){
     console.log('DB connected');
@@ -26,8 +26,8 @@ app.set('view engine', 'ejs');
 app.use(express.static(__dirname+'/public'));
 
 //Port setting
-//const PORT = process.env.PORT || 4000; //못 찾으면 4000번으로
-const PORT = 4000; //못 찾으면 4000번으로
+const PORT = process.env.PORT || 4000; //못 찾으면 4000번으로
+// const PORT = 4000; 
 
 app.listen(PORT, function(){
     console.log('server on! http://localhost:'+PORT);
