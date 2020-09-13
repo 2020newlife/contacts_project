00bedc72e817bbbda97e762da8684ae06bf9305f
ilsun <naiseya486@gmail.com>
2020-09-13 20:45:14 +0900

    1index.js - mongodb schema, collection, model setting 2)get, post router setting

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var mongoose = require('mongoose');
 var dotenv = require('dotenv');
-dotenv.config();
+var bodyParser = require('body-parser');
 var app = express();
+dotenv.config();
 
 //DB setting 
 mongoose.set('useNewUrlParser', true);
@@ -11,7 +12,6 @@ mongoose.set('useCreateIndex', true);
 mongoose.set('useUnifiedTopology', true);
 
 mongoose.connect(process.env.MONGO_URL);
-// mongoose.connect("mongodb://127.0.0.1:27017/contacts_db");
 var db = mongoose.connection;
 db.once('open', function(){
     console.log('DB connected');
@@ -24,10 +24,47 @@ db.on('error', function(){
 //Other settings
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname+'/public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+
+// DB schema
+var contactSchema = mongoose.Schema({
+    name:{type:String, required:true, unique:true},
+    email:{type:String},
+    phone:{type:String}
+});
+
+var Contact = mongoose.model('contact', contactSchema);
+
+// Routes
+// Home 
+app.get('/', function(req, res){
+    res.redirect('/contacts');
+});
+
+// Contacts - Index 
+app.get('/contacts', function(req, res{
+    Contact.find({}, function(err, contacts){
+        if(err) return res.json(err);
+        res.render('contacts/index', {contacts:contacts});
+    });
+}));
+
+// Contacts - New
+app.get('/contacts/new', function(req, res){
+    res.render('contacts/new');
+});
+
+// Contacts - create
+app.post('/contacts', function(req, res){
+    Contact.create(req.body, function(err, contact){
+        if(err) return res.json(err);
+        res.redirect('/contacts');
+    });
+});
 
 //Port setting
 const PORT = process.env.PORT || 4000; //못 찾으면 4000번으로
-// const PORT = 4000; 
 
 app.listen(PORT, function(){
     console.log('server on! http://localhost:'+PORT);
